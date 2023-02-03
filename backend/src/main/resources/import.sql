0c41f379336ad49494fda68cbac43db0147c1a2e
maririb749 <maririb51@gmail.com>
2023-02-03 20:14:39 +0000

    Entidade Client

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,27 @@
+
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Ana Maria', '217.268.197-10', 2600.00, '1982-10-20', 3);
+
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Carlos Silva', '314.666.000-10', 3400.00, '1998-11-04', 2);
+
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Angela Pereira', '415.854.222-00', 1300.00, '1976-01-05', 1);
+
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Sergio Santos', '999.777.222-11', 3200.00, '1989-07-21', 4);
+
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Claudia Leite', '000.111.333-10', 4700.00, '1990-03-02', 5);
+
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Paula Barreto', '212.414.717-05', 1100.00, '2000-04-18', 4);
+
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Carolina Jesus', '344.822.440-00', 2600.00, '1976-07-11', 8);
+
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Roberta Carla', '000.111.999-87', 1800.00, '1987-01-03', 2);
+
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Sonia Aragão', '217.268.197-10', 2600.00, '1982-10-20', 3);
+
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Paula Correia', '217.268.197-10', 7600.00, '1982-10-20', 6);
+
+
+
+
+
+
+
